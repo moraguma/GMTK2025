4d915e7c1d2c22f32ebd75d6c374ad6e09cf4d59
moraguma <guambbe@gmail.com>
2025-08-02 17:10:08 -0300

    Adds rocks

--- scenes/scripts/Destructible.gd
+++ scenes/scripts/Destructible.gd
@@ -3,9 +3,19 @@ class_name Destructible
 
 
 @export var sound: String
+@onready var sprite: Sprite2D = $Sprite
+
+
+var destroyed = false
 
 
 func destroy():
-	queue_free()
+	if destroyed:
+		return
+	destroyed = true
+	
+	$Hitbox.queue_free()
+	$Particles.emitting = true
+	sprite.hide()
 	
 	SoundController.play_sfx(sound)
